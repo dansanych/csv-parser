761fde1ff1bdd08d6f6316336d14a9d4c36bf8ba
Danil Dudnik <danil.dudnik@gmail.com>
2022-05-01 14:01:30 +0300

    Initial commit

---
+++ src/com/company/CityTools.java
@@ -0,0 +1,51 @@
+package com.company;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+
+public class CityTools {
+
+    private static final String FILEPATH = "src/resources/city_ru.csv";
+
+    public static List<City> parse() throws FileNotFoundException {
+        List<City> cityList = new ArrayList<>();
+
+        File file = new File(FILEPATH);
+        Scanner scanner = new Scanner(file);
+
+        while (scanner.hasNextLine()) {
+            cityList.add(fillObject(scanner.nextLine()));
+        }
+
+        return cityList;
+    }
+
+    public static City fillObject(String line) {
+        Scanner scanner = new Scanner(line);
+        scanner.useDelimiter(";");
+        scanner.skip("\\d*");
+
+        String name = scanner.next();
+        String region = scanner.next();
+        String district = scanner.next();
+        int population = scanner.nextInt();
+        String foundation = null;
+        if (scanner.hasNext()) {
+            foundation = scanner.next();
+        }
+
+        scanner.close();
+
+        return new City(name, region, district, population, foundation);
+    }
+
+    public static void output(List<City> cityList) {
+        for (City city : cityList) {
+            System.out.println(city);
+        }
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+
+import java.io.FileNotFoundException;
+
+import static com.company.CityTools.output;
+import static com.company.CityTools.parse;
+
+public class Main {
+
+    public static void main(String[] args) throws FileNotFoundException {
+        // write your code here
+        output(parse());
+    }
+}
